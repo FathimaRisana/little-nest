1724af6581bf3ba9afd45b8e49f653ee1458ddef
Bhusthana Shiren <bhusthanashirenpp@gmail.com>
2026-07-18 13:58:12 +0530

    Configure environment variables and Python version for Render deployment

--- ecombabyshoppingproject/ecombabyshoppingproject/settings.py
+++ ecombabyshoppingproject/ecombabyshoppingproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ddh%#5&6m%c-0euywpwve=2#=vv*l=jr1@-6py*s$ks)&@^l$q'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ddh%#5&6m%c-0euywpwve=2#=vv*l=jr1@-6py*s$ks)&@^l$q')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 'yes')
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost', '*']
 
